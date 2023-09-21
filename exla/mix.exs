1cf63bfa602688e810c0ec9cc48a2d360979b310
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2023-09-21 10:16:38 +0100

    Update mix.exs to remove alt versions for telemetry dep
    rebar_mix fails otherwise

--- exla/mix.exs
+++ exla/mix.exs
@@ -61,7 +61,7 @@ defmodule EXLA.MixProject do
     [
       # {:nx, "~> 0.6.0"},
       {:nx, path: "../nx"},
-      {:telemetry, "~> 0.4.0 or ~> 1.0"},
+      {:telemetry, "~> 1.0"},
       {:xla, "~> 0.5.0", runtime: false},
       {:elixir_make, "~> 0.6", runtime: false},
       {:benchee, "~> 1.0", only: :dev},
