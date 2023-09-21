de8419344726964f44a0861ba3ecd091a9c67cd2
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2023-09-21 10:05:55 +0100

    Update mix.exs to remove alt versions for telemetry depending
    Erlang rebar_mix cannot handle alt versiones

--- nx/mix.exs
+++ nx/mix.exs
@@ -40,7 +40,7 @@ defmodule Nx.MixProject do
   defp deps do
     [
       {:complex, "~> 0.5"},
-      {:telemetry, "~> 0.4.0 or ~> 1.0"},
+      {:telemetry, "~> 1.0"},
       {:ex_doc, "~> 0.29", only: :docs}
     ]
   end
